5d7666a7d4d63eeb72a30e40475dfcad819635cb
Ben Collier <bcollier@biolinux-Latitude-E6320>
2015-02-06 09:35:57 +0000

    Adding server listings, stop, start and ajax updating.

--- eos_portal/__init__.py
+++ eos_portal/__init__.py
@@ -1,28 +1,33 @@
 from pyramid.config import Configurator
-from pyramid.session import UnencryptedCookieSessionFactoryConfig
-my_session_factory = UnencryptedCookieSessionFactoryConfig('itsasekret')
-
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
-from .security import groupfinder
+from pyramid.session import UnencryptedCookieSessionFactoryConfig
+
+from eos_portal.security import groupfinder
+
+my_session_factory = UnencryptedCookieSessionFactoryConfig('itsasekret')
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    authn_policy = AuthTktAuthenticationPolicy(
+        'sosecret', callback=groupfinder, hashalg='sha512')
+    authz_policy = ACLAuthorizationPolicy()
+    
     config = Configurator(settings=settings, session_factory=my_session_factory)
+
+    config.set_authentication_policy(authn_policy)
+    config.set_authorization_policy(authz_policy)
+    
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
-    config.add_route('main', '/main')
-    
-    authn_policy = AuthTktAuthenticationPolicy(
-        'sosecret', callback=groupfinder, hashalg='sha512') # To be sorted
-    authz_policy = ACLAuthorizationPolicy()
-
-    config.set_authentication_policy(authn_policy)
-    config.set_authorization_policy(authz_policy)
-    
+    config.add_route('servers', '/servers')
+    config.add_route('stop', '/stop')
+    config.add_route('account', '/account')
+    config.add_route('forbidden', '/forbidden')
+  
     config.scan()
     return config.make_wsgi_app()
--- eos_portal/security.py
+++ eos_portal/security.py
@@ -1,5 +1,2 @@
 def groupfinder(userid, request):
-    if len(userid) > 0:
-        return "user"
-    else:
-        return "unauthorised"
+    return ['user']
