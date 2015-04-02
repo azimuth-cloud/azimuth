22c1efca163d798a2c1684aff418d6076b8bfad4
Ben Collier <bmcollier@gmail.com>
2015-04-02 16:07:46 +0100

    Application of pass-through security model to portal.

--- eos_portal/__init__.py
+++ eos_portal/__init__.py
@@ -2,23 +2,17 @@ from pyramid.config import Configurator
 from pyramid.authentication import AuthTktAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
+from pyramid.events import NewRequest, NewResponse
 
 from eos_portal.security import groupfinder
 
-my_session_factory = UnencryptedCookieSessionFactoryConfig('itsasekret')
+my_session_factory = UnencryptedCookieSessionFactoryConfig('asdfasdfas')
 
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
-    authn_policy = AuthTktAuthenticationPolicy(
-        'sosecret', callback=groupfinder, hashalg='sha512')
-    authz_policy = ACLAuthorizationPolicy()
-    
     config = Configurator(settings=settings, session_factory=my_session_factory)
 
-    config.set_authentication_policy(authn_policy)
-    config.set_authorization_policy(authz_policy)
-    
     config.include('pyramid_chameleon')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
@@ -29,6 +23,6 @@ def main(global_config, **settings):
     config.add_route('stop', '/stop')
     config.add_route('account', '/account')
     config.add_route('forbidden', '/forbidden')
-  
+
     config.scan()
     return config.make_wsgi_app()
