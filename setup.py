024410e8037df7424c32f8e579c721abe0cf2b72
Tim Booth <tbooth@ceh.ac.uk>
2015-05-15 16:24:47 +0100

    Changes to setup.py in line with eod_db

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 import os
 
 from setuptools import setup, find_packages
@@ -7,37 +8,42 @@ with open(os.path.join(here, 'README.md')) as f:
     README = f.read()
 with open(os.path.join(here, 'CHANGES.md')) as f:
     CHANGES = f.read()
+with open(os.path.join(here, 'VERSION')) as f:
+    VERSION = f.read().split()[0]
 
 requires = [
     'pyramid',
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'waitress',
-    'requests'
+    'requests',
+    'webtest',     # Needed to run the tests
     ]
 
-setup(name='eos_portal',
-      version='0.0',
-      description='eos_portal',
-      long_description=README + '\n\n' + CHANGES,
-      classifiers=[
-        "Programming Language :: Python",
-        "Framework :: Pyramid",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        ],
-      author='',
-      author_email='',
-      url='',
-      keywords='web pyramid pylons',
-      packages=find_packages(),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=requires,
-      tests_require=requires,
-      test_suite="eos_portal",
-      entry_points="""\
-      [paste.app_factory]
-      main = eos_portal:main
-      """,
-      )
+if __name__ == "__main__":
+
+    setup(name='eos_portal',
+          version=VERSION,
+          description='eos_portal',
+          long_description=README + '\n\n' + CHANGES,
+          classifiers=[
+            "Programming Language :: Python",
+            "Framework :: Pyramid",
+            "Topic :: Internet :: WWW/HTTP",
+            "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+            ],
+          author='Benjamin Collier, Tim Booth',
+          author_email='bmcollier@gmail.com, tbooth@ceh.ac.uk',
+          url='http://eoscloud.nerc.ac.uk',
+          keywords='web pyramid pylons',
+          packages=find_packages(),
+          include_package_data=True,
+          zip_safe=False,
+          install_requires=requires,
+          tests_require=requires,
+          test_suite="eos_portal.test",
+          entry_points="""\
+          [paste.app_factory]
+          main = eos_portal:main
+          """,
+          )
