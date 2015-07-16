74492e1819352af2f43b1c185b9a9ed1aff80af9
Tim Booth <tbooth@ceh.ac.uk>
2015-07-16 15:26:25 +0100

    Fix for Internet Exploder

---
+++ eos_portal/static/js/local-fixups.js
@@ -0,0 +1,7 @@
+// This patches a shortcoming of IE.
+
+if (typeof String.prototype.startsWith != 'function') {
+    String.prototype.startsWith = function (str){
+	return this.slice(0, str.length) == str;
+    };
+}
