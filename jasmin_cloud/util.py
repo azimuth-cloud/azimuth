159e933a713f8f7884c4c6b6ad4780ba07349e70
Matt Pryor <matt.pryor@stfc.ac.uk>
2015-10-27 10:43:47 +0000

    Removed no longer used utilities

--- jasmin_cloud/util.py
+++ jasmin_cloud/util.py
@@ -6,67 +6,9 @@ __author__ = "Matt Pryor"
 __copyright__ = "Copyright 2015 UK Science and Technology Facilities Council"
 
 
-import os, tempfile, subprocess, re
-from functools import reduce
-from collections import Iterable
+import os, tempfile, subprocess
 
 
-def first(iterable, default):
-    """
-    Returns the first element of the iterable if it is non-empty, or the given
-    default if it is empty.
-    
-    :param iterable: The iterable to get the first element of
-    :param default: The default value to return if the iterable is empty
-    :returns: The first element of iterable or default
-    """
-    return next(iter(iterable), default)
-    
-
-def getattrs(obj, attrs, default):
-    """
-    Similar to ``getattr``, but allows a list of attribute names to be resolved
-    in a chained manner.
-    
-    If an attribute cannot be resolved at any point in the chain, the default is
-    returned.
-    
-    :param obj: An object to start resolving names from
-    :param attrs: List of names to resolve
-    :param default: Default value if attribute resolution fails at any stage
-    :returns: The value at the end of the chain if resolution is successful,
-              otherwise the default
-    """
-    try:
-        return reduce(getattr, attrs, obj)
-    except (KeyError, AttributeError):
-        return default
-    
-
-# Lifted from formencode via validino
-_usernameRE = re.compile(r"^[^ \t\n\r@<>()]+$", re.I)
-_domainRE = re.compile(r"^[a-z0-9][a-z0-9\.\-_]*\.[a-z]+$", re.I)
-
-def validate_email(email):
-    """
-    Verifies that the given value is a valid email address.
-    
-    Returns the value on success, raises ``ValueError`` on failure.
-    
-    :param email: The value to test
-    :returns: The value on success
-    """
-    try:
-        username, domain = email.split('@', 1)
-    except ValueError:
-        raise ValueError('Not a valid email address')
-    if not _usernameRE.match(username):
-        raise ValueError('Not a valid email address')
-    if not _domainRE.match(domain):
-        raise ValueError('Not a valid email address')
-    return email
-    
-    
 def validate_ssh_key(ssh_key):
     """
     Verifies that the given value is a valid SSH key.
@@ -94,23 +36,3 @@ def validate_ssh_key(ssh_key):
     except subprocess.CalledProcessError:
         raise ValueError('Not a valid SSH key')
     return ssh_key
-    
-    
-class DeferredIterable(Iterable):
-    """
-    Iterable decorator where the creation of the underlying iterable is deferred
-    until data is first requested.
-    
-    :param factory: Callable that creates the underlying iterable. Should take no
-                    arguments and return an iterable
-    """
-    def __init__(self, factory):
-        self._factory = factory
-        self._underlying = None
-        
-    def __iter__(self):
-        if self._underlying is None:
-            self._underlying = self._factory()
-            self._factory = None
-        return iter(self._underlying)
-    
