9e4696aa634ab9b2ee85e60beca9ee1977a0b355
Matt Pryor <matt.pryor@stfc.ac.uk>
2016-03-10 11:35:11 +0000

    Fix error when matchdict is None

--- jasmin_cloud/auth.py
+++ jasmin_cloud/auth.py
@@ -20,11 +20,11 @@ from .cloudservices import CloudServiceError
 def includeme(config):
     """
     Configures the Pyramid application for authentication and authorization.
-    
+
     :param config: Pyramid configurator
     """
     settings = config.get_settings()
-    
+
     # We want to use token based authentication, with a check on the cloud sessions
     config.set_authentication_policy(AuthTktAuthenticationPolicy(
         settings['auth.secret'], hashalg = 'sha512', callback = _check_cloud_sessions
@@ -32,17 +32,17 @@ def includeme(config):
     # We use a basic ACL policy for authorisation
     config.set_authorization_policy(ACLAuthorizationPolicy())
     config.set_root_factory(RootFactory)
-    
+
     # Make a user manager available for the request
     config.add_request_method(users, reify = True)
-    
+
     # Make (un)authenticated_user properties available on the request object that
     # return a JASMIN user object
     config.add_request_method(authenticated_user, reify = True)
 
     # Make the current organisation name available as a property of the request
     config.add_request_method(current_org, reify = True)
-    
+
     # Inject the org from the current request into route_url and route_path if
     # not overridden
     # Because we need access to the original versions of the functions to defer
@@ -52,32 +52,32 @@ def includeme(config):
             if 'org' not in kw and request.current_org:
                 kw['org'] = request.current_org
             return kw
-        
+
         # Overwrite the route_url and route_path functions with versions that inject the org
         request = event.request
-        
+
         route_url = request.route_url
         request.route_url = lambda route, *args, **kw: \
             route_url(route, *args, **inject_org(request, kw))
-        
+
         route_path = request.route_path
         request.route_path = lambda route, *args, **kw: \
             route_path(route, *args, **inject_org(request, kw))
     config.add_subscriber(overwrite_path_funcs, events.NewRequest)
-    
-    
+
+
 def users(request):
     """
     Returns a ``jasmin_auth.UserManager`` configured using settings from the given
     request.
-    
+
     .. note::
-    
+
         This function should be accessed as a property of the Pyramid request object,
         i.e. ``users = request.users``.
-       
+
         This property is reified, so it is only evaluated once per request.
-       
+
     :param request: The Pyramid request
     :returns: A ``jasmin_auth.UserManager``
     """
@@ -93,14 +93,14 @@ def authenticated_user(request):
     """
     Returns a ``jasmin_auth.User`` based on the ``authenticated_userid`` of the
     given request.
-    
+
     .. note::
-    
+
         This function should be accessed as a property of the Pyramid request object,
         i.e. ``user = request.authenticated_user``.
-       
+
         This property is reified, so it is only evaluated once per request.
-       
+
     :param request: The Pyramid request
     :returns: A ``jasmin_auth.User`` or ``None``
     """
@@ -108,38 +108,38 @@ def authenticated_user(request):
         return request.users.find_by_username(request.authenticated_userid)
     else:
         return None
-    
+
 
 def current_org(request):
     """
     Returns the organisation name in the URL of the given request, or ``None`` if
     we are not on an organisation-specific URL.
-    
+
     .. note::
-    
+
         This function should be accessed as a property of the Pyramid request object,
         i.e. ``org = request.current_org``.
-       
+
         This property is reified, so it is only evaluated once per request.
-       
+
     :param request: The Pyramid request
     :returns: The organisation name or ``None``
     """
     try:
         return request.matchdict['org'].lower()
-    except (AttributeError, KeyError):
+    except (TypeError, AttributeError, KeyError):
         return None
 
 
 def _check_cloud_sessions(userid, request):
     """
     Group finder for use with the Pyramid ``AuthTktAuthenticationPolicy``.
-    
+
     Returns the list of organisation names to which the current user has access.
-    
+
     To do this, we use ``request.cloud_sessions``, and assume it contains a session
     per organisation for which the user has access.
-    
+
     If one of the sessions has timed out, we treat the user as completely
     unauthenticated, and force them to log in again.
     """
@@ -162,12 +162,12 @@ def _check_cloud_sessions(userid, request):
 class RootFactory:
     """
     Provides the ACL for use with the Pyramid ``ACLAuthorizationPolicy``
-    
+
     The ACL is dynamically generated based on the current request so that only
     members of the organisation in the URL are granted access to
     organisation-specific pages.
     """
-    
+
     def __init__(self, request):
         # If there is an org in the URI, members of that org are granted the
         # org_view and org_edit privileges
