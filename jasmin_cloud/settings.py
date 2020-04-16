6a304da8be1cd2bc2a1ca11c1953958a9ad28bca
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-04-16 09:32:19 +0100

    Remove the requirement for a database

--- jasmin_cloud/settings.py
+++ jasmin_cloud/settings.py
@@ -4,13 +4,15 @@ Settings helpers for the ``jasmin_cloud`` Django app.
 
 from django.conf import settings
 
-from settings_object import SettingsObject, ObjectFactorySetting
+from settings_object import SettingsObject, Setting, ObjectFactorySetting
 
 
 class JasminCloudSettings(SettingsObject):
     """
     Settings object for the ``JASMIN_CLOUD`` setting.
     """
+    #: The name of the cookie used to store tokens
+    TOKEN_COOKIE_NAME = Setting(default = 'provider-token')
     #: Cloud provider configuration
     PROVIDER = ObjectFactorySetting()
     #: SSH key store configuration
--- jasmin_cloud_site/urls.py
+++ jasmin_cloud_site/urls.py
@@ -3,10 +3,8 @@ Root URL configuration for the JASMIN Cloud API site.
 """
 
 from django.conf.urls import url, include
-from django.contrib import admin
 
 
 urlpatterns = [
     url(r'^api/', include('jasmin_cloud.urls', namespace = 'jasmin_cloud')),
-    url(r'^admin/', admin.site.urls),
 ]
