7b4f45a89b47a484265386da2b4cbf371a2b4c0b
Matt Pryor <matt.pryor@stfc.ac.uk>
2020-04-16 11:31:52 +0100

    Add simple healthcheck endpoint

--- jasmin_cloud_site/urls.py
+++ jasmin_cloud_site/urls.py
@@ -2,9 +2,22 @@
 Root URL configuration for the JASMIN Cloud API site.
 """
 
-from django.conf.urls import url, include
+from django.urls import path, include
+from django.http import HttpResponse
+
+
+def status(request):
+    """
+    Endpoint used for healthchecks.
+    """
+    # Just return 204 No Content
+    return HttpResponse(status = 204)
 
 
 urlpatterns = [
-    url(r'^api/', include('jasmin_cloud.urls', namespace = 'jasmin_cloud')),
+    # Install a URL to use for health checks
+    # We can't use any of the /api URLs as they either require authentication
+    # or don't accept use of the GET method
+    path('_status/', status, name = 'status'),
+    path('api/', include('jasmin_cloud.urls', namespace = 'jasmin_cloud')),
 ]
