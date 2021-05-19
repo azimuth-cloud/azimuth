f6856677e69bfaa99dcd81c98f9e7b3794c743c2
Matt Pryor <mkjpryor@gmail.com>
2021-05-19 17:40:49 +0100

    Formalise dummy keystore implementation

--- jasmin_cloud/keystore/dummy.py
+++ jasmin_cloud/keystore/dummy.py
@@ -1,5 +1,14 @@
-class KeyStore(object):
-    def __init__(self, key=None):
+from .base import KeyStore
+
+
+class DummyKeyStore(KeyStore):
+    """
+    Key store implementation that returns a single key for all users.
+
+    Args:
+        key: The single public key to use for all users.
+    """
+    def __init__(self, key = None):
         self.key = key
 
     def get_key(self, username):
