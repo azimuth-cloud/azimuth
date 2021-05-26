17d30977ea32fc0f89f0b2fa98d9a812716ba913
Matt Pryor <mkjpryor@gmail.com>
2021-05-26 09:51:50 +0100

    Update settings to reflect changes

--- etc/django/app.py
+++ etc/django/app.py
@@ -1,6 +1,25 @@
-# Application definition
+"""
+Application-specific settings.
+"""
+
+# Remove unnecessary context processors to maybe reduce some overhead
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
+
 INSTALLED_APPS = [
     'django.contrib.staticfiles',
+    'cloud_auth',
     'jasmin_cloud',
     'rest_framework',
 ]
@@ -10,18 +29,21 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'jasmin_cloud.middleware.provider_session',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'cloud_auth.middleware.SessionTokenMiddleware',
+    'jasmin_cloud.middleware.CleanupProviderMiddleware',
 ]
 
 ROOT_URLCONF = 'jasmin_cloud_site.urls'
 
 WSGI_APPLICATION = 'jasmin_cloud_site.wsgi.application'
 
+# Use cookie sessions so that we don't need a database
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+
 REST_FRAMEWORK = {
     'VIEW_DESCRIPTION_FUNCTION': 'jasmin_cloud.views.get_view_description',
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'jasmin_cloud.authentication.TokenCookieAuthentication',
-    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': ['jasmin_cloud.authentication.TokenHeaderAuthentication'],
     'UNAUTHENTICATED_USER': None,
 }
 
