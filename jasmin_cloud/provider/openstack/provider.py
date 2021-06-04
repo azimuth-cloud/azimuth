bf3d02a77264aedd04e093bd99e3b3a7723aa09c
Matt Pryor <mkjpryor@gmail.com>
2021-06-04 16:20:03 +0100

    Add support for using tagged networks

--- jasmin_cloud/provider/openstack/provider.py
+++ jasmin_cloud/provider/openstack/provider.py
@@ -117,7 +117,6 @@ class Provider(base.Provider):
                        domain = 'Default',
                        interface = 'public',
                        az_backdoor_net_map = dict(),
-                       net_device_owner = 'network:router_interface',
                        backdoor_vnic_type = None,
                        verify_ssl = True,
                        cluster_engine = None):
@@ -126,7 +125,6 @@ class Provider(base.Provider):
         self._domain = domain
         self._interface = interface
         self._az_backdoor_net_map = az_backdoor_net_map or dict()
-        self._net_device_owner = net_device_owner
         self._backdoor_vnic_type = backdoor_vnic_type
         self._verify_ssl = verify_ssl
         self._cluster_engine = cluster_engine
@@ -149,7 +147,6 @@ class Provider(base.Provider):
             return UnscopedSession(
                 conn,
                 az_backdoor_net_map = self._az_backdoor_net_map,
-                net_device_owner = self._net_device_owner,
                 backdoor_vnic_type = self._backdoor_vnic_type,
                 cluster_engine = self._cluster_engine
             )
@@ -170,7 +167,6 @@ class Provider(base.Provider):
             return UnscopedSession(
                 conn,
                 az_backdoor_net_map = self._az_backdoor_net_map,
-                net_device_owner = self._net_device_owner,
                 backdoor_vnic_type = self._backdoor_vnic_type,
                 cluster_engine = self._cluster_engine
             )
@@ -195,13 +191,11 @@ class UnscopedSession(base.UnscopedSession):
     provider_name = 'openstack'
 
     def __init__(self, connection,
-                       net_device_owner = 'network:router_interface',
                        az_backdoor_net_map = None,
                        backdoor_vnic_type = None,
                        cluster_engine = None):
         self._connection = connection
         self._az_backdoor_net_map = az_backdoor_net_map or dict()
-        self._net_device_owner = net_device_owner
         self._backdoor_vnic_type = backdoor_vnic_type
         self._cluster_engine = cluster_engine
 
@@ -248,7 +242,6 @@ class UnscopedSession(base.UnscopedSession):
                 tenancy,
                 self._connection.scoped_connection(tenancy.id),
                 az_backdoor_net_map = self._az_backdoor_net_map,
-                net_device_owner = self._net_device_owner,
                 backdoor_vnic_type = self._backdoor_vnic_type,
                 cluster_engine = self._cluster_engine
             )
@@ -289,14 +282,12 @@ class ScopedSession(base.ScopedSession):
                        tenancy,
                        connection,
                        az_backdoor_net_map = None,
-                       net_device_owner = 'network:router_interface',
                        backdoor_vnic_type = None,
                        cluster_engine = None):
         self._username = username
         self._tenancy = tenancy
         self._connection = connection
         self._az_backdoor_net_map = az_backdoor_net_map or dict()
-        self._net_device_owner = net_device_owner
         self._backdoor_vnic_type = backdoor_vnic_type
         self._cluster_engine = cluster_engine
 
@@ -443,13 +434,27 @@ class ScopedSession(base.ScopedSession):
         self._log("Fetching flavor with id '%s'", id)
         return self._from_api_flavor(self._connection.compute.flavors.get(id))
 
+    def _tagged_network(self, tag):
+        """
+        Returns the first network with the given tag, or None if there is not one.
+        """
+        return next(self._connection.network.networks.all(tags = tag), None)
+
     def _tenant_network(self):
         """
         Returns the network connected to the tenant router.
         Assumes a single router with a single tenant network connected.
         """
+        # First, try to find a network that is tagged as the portal internal network
+        tagged_network = self._tagged_network('portal-internal')
+        if tagged_network:
+            self._log("Using tagged internal network: {}".format(tagged_network.name))
+            return tagged_network
+        else:
+            self._log("Failed to find tagged internal network.")
+        # If there are no networks with that tag, try to find the network using the tenancy router
         # Find the port that connects the tenancy network to a router
-        port = self._connection.network.ports.find_by_device_owner(self._net_device_owner)
+        port = self._connection.network.ports.find_by_device_owner('network:router_interface')
         if port:
             # Get the network that is attached to that port
             return self._connection.network.networks.get(port.network_id)
@@ -460,6 +465,14 @@ class ScopedSession(base.ScopedSession):
         """
         Returns the external network that connects the tenant router to the outside world.
         """
+        # First, try to find a network that is tagged as the portal external network
+        tagged_network = self._tagged_network('portal-external')
+        if tagged_network:
+            self._log("Using tagged external network: {}".format(tagged_network.name))
+            return tagged_network
+        else:
+            self._log("Failed to find tagged external network.")
+        # Otherwise find the external gateway for the tenancy router
         try:
             # Try to get the router for the tenancy
             router = next(self._connection.network.routers.all())
