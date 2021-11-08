807ea862c7be6a710c47db64fc31fced6b3a18d7
Matt Pryor <matt@stackhpc.com>
2021-11-08 10:38:21 +0000

    Allow domains rather than only hosts for redirect URL check

--- api/azimuth_auth/settings.py
+++ api/azimuth_auth/settings.py
@@ -5,7 +5,7 @@ Settings for the Azimuth auth package.
 from settings_object import SettingsObject, Setting, MergedDictSetting, ObjectFactorySetting
 
 
-class CloudAuthSettings(SettingsObject):
+class AzimuthAuthSettings(SettingsObject):
     """
     Settings object for the ``AZIMUTH_AUTH`` setting.
     """
@@ -13,40 +13,40 @@ class CloudAuthSettings(SettingsObject):
     AUTHENTICATOR = ObjectFactorySetting()
 
     #: The name of the header in which to place the token for downstream code
-    DOWNSTREAM_TOKEN_HEADER = Setting(default = 'HTTP_X_CLOUD_TOKEN')
+    DOWNSTREAM_TOKEN_HEADER = Setting(default = "HTTP_X_CLOUD_TOKEN")
 
     #: For the bearer token middleware, this is the name of the header that the token will be in
-    BEARER_TOKEN_HEADER = Setting(default = 'HTTP_AUTHORIZATION')
+    BEARER_TOKEN_HEADER = Setting(default = "HTTP_AUTHORIZATION")
     #: For the bearer token middleware, this is the prefix that will be present and needs to be stripped
-    BEARER_TOKEN_PREFIX = Setting(default = 'Bearer')
+    BEARER_TOKEN_PREFIX = Setting(default = "Bearer")
 
     #: For the session middleware, this is the name of the session key in which the token is stored
-    TOKEN_SESSION_KEY = Setting(default = 'token')
+    TOKEN_SESSION_KEY = Setting(default = "token")
     #: For the session middleware, this is the amount of time, in seconds, before the token expires that
     #: middleware will attempt to refresh it
     #: Defaults to 10 minutes if not given
     TOKEN_REFRESH_INTERVAL = Setting(default = 600)
 
     #: The HTTP parameter to get the next URL from
-    NEXT_URL_PARAM = Setting(default = 'next')
+    NEXT_URL_PARAM = Setting(default = "next")
     #: The session key to store the next URL
-    NEXT_URL_COOKIE_NAME = Setting(default = 'next-url')
-    #: The allowed hosts for the next URL
-    NEXT_URL_ALLOWED_HOSTS = Setting(default = set)
+    NEXT_URL_COOKIE_NAME = Setting(default = "next-url")
+    #: The allowed domains for the next URL
+    NEXT_URL_ALLOWED_DOMAINS = Setting(default = set)
     #: The default next URL if the user-supplied URL is not given or not permitted
-    NEXT_URL_DEFAULT_URL = Setting(default = '/dashboard')
+    NEXT_URL_DEFAULT_URL = Setting(default = "/dashboard")
 
     #: The HTTP parameter to get the message code from
-    MESSAGE_CODE_PARAM = Setting(default = 'code')
+    MESSAGE_CODE_PARAM = Setting(default = "code")
     #: The default message level
-    DEFAULT_MESSAGE_LEVEL = Setting(default = 'danger')
+    DEFAULT_MESSAGE_LEVEL = Setting(default = "danger")
     #: The messages to use for each code
     #: Each item can be either a string or a dict with a string and a level
     MESSAGES = MergedDictSetting({
-        'session_expired': 'Your session has expired. Please sign in again.',
-        'invalid_credentials': 'The provided credentials are invalid. Please try again.',
-        'logout_successful': { 'message': 'You have been signed out.', 'level': 'success' },
+        "session_expired": "Your session has expired. Please sign in again.",
+        "invalid_credentials": "The provided credentials are invalid. Please try again.",
+        "logout_successful": { "message": "You have been signed out.", "level": "success" },
     })
 
 
-auth_settings = CloudAuthSettings('AZIMUTH_AUTH')
+auth_settings = AzimuthAuthSettings("AZIMUTH_AUTH")
--- api/azimuth_auth/views.py
+++ api/azimuth_auth/views.py
@@ -2,15 +2,61 @@
 Views for the Azimuth auth package.
 """
 
-from urllib.parse import urlencode
+import unicodedata
+from urllib.parse import urlparse, urlencode
 
 from django.shortcuts import redirect, render
 from django.urls import reverse
-from django.utils.http import url_has_allowed_host_and_scheme
 from django.views.decorators.csrf import csrf_exempt, csrf_protect
 from django.views.decorators.http import require_http_methods, require_safe
 
 from .settings import auth_settings
+
+
+def url_has_allowed_host_and_scheme(url, allowed_domains, require_https = False):
+    """
+    Modified version of Django's url_has_allowed_host_and_scheme that allows us to validate
+    against wildcard domains.
+
+    https://github.com/django/django/blob/ba9ced3e9a643a05bc521f0a2e6d02e3569de374/django/utils/http.py#L239
+    """
+    if url is not None:
+        url = url.strip()
+    if not url:
+        return False
+    # Chrome considers any URL with more than two slashes to be absolute, but
+    # urlparse is not so flexible. Treat any url with three slashes as unsafe.
+    if url.startswith("///"):
+        return False
+    try:
+        url_info = urlparse(url)
+    except ValueError:  # e.g. invalid IPv6 addresses
+        return False
+    # Forbid URLs like http:///example.com - with a scheme, but without a hostname.
+    # In that URL, example.com is not the hostname but, a path component. However,
+    # Chrome will still consider example.com to be the hostname, so we must not
+    # allow this syntax
+    if not url_info.netloc and url_info.scheme:
+        return False
+    # Forbid URLs that start with control characters. Some browsers (like
+    # Chrome) ignore quite a few control characters at the start of a
+    # URL and might consider the URL as scheme relative
+    if unicodedata.category(url[0])[0] == "C":
+        return False
+    scheme = url_info.scheme
+    # Consider URLs without a scheme (e.g. //example.com/p) to be http
+    if not url_info.scheme and url_info.netloc:
+        scheme = "http"
+    # Reject URLs that have a scheme that we don't like
+    valid_schemes = ["https"] if require_https else ["http", "https"]
+    if scheme and scheme not in valid_schemes:
+        return False
+    # If we get this far, check the host
+    # First, we allow any relative URLs
+    if not url_info.netloc:
+        return True
+    # Then check whether the host is in any of our domains
+    return any(url_info.netloc.endswith(domain) for domain in allowed_domains)
 
 
 def get_next_url(request):
@@ -26,7 +72,7 @@ def get_next_url(request):
         next_url = request.get_signed_cookie(auth_settings.NEXT_URL_COOKIE_NAME, None)
     url_is_safe = url_has_allowed_host_and_scheme(
         url = next_url,
-        allowed_hosts = { request.get_host(), *auth_settings.NEXT_URL_ALLOWED_HOSTS },
+        allowed_domains = { request.get_host(), *auth_settings.NEXT_URL_ALLOWED_DOMAINS },
         require_https = request.is_secure(),
     )
     return next_url if url_is_safe else auth_settings.NEXT_URL_DEFAULT_URL
@@ -39,7 +85,7 @@ def set_next_url_cookie(response, next_url, secure):
     We use a separate cookie rather than using the session because in order for some
     authenticators to redirect correctly (e.g. OpenStack federation) we need to be able
     to access the next URL from a cross-domain POST request. This requires storing
-    the URL in a cookie with SameSite='None', which we don't want to do with the
+    the URL in a cookie with SameSite="None", which we don't want to do with the
     session cookie. However the next URL is not sensitive data, and is checked for safety
     anyway, so it can go in a cross-domain cookie.
     """
@@ -50,9 +96,9 @@ def set_next_url_cookie(response, next_url, secure):
             httponly = True,
             secure = secure,
             samesite = (
-                'None'
+                "None"
                 if auth_settings.AUTHENTICATOR.uses_crossdomain_post_requests
-                else 'Lax'
+                else "Lax"
             )
         )
     else:
@@ -64,7 +110,7 @@ def redirect_to_login(code = None):
     """
     Redirect to the login endpoint with an optional code.
     """
-    redirect_to = reverse('azimuth_auth:login')
+    redirect_to = reverse("azimuth_auth:login")
     if code:
         redirect_to = "{}?{}".format(
             redirect_to,
@@ -101,12 +147,12 @@ def complete(request):
             if next_url:
                 response = redirect(next_url)
             else:
-                response = render(request, 'azimuth_auth/complete.html')
+                response = render(request, "azimuth_auth/complete.html")
             # On a successful authentication, we also want to clear the next URL cookie
             return set_next_url_cookie(response, None, request.is_secure())
         else:
             # On a failed authentication, redirect to login but indicate that the auth failed
-            return redirect_to_login('invalid_credentials')
+            return redirect_to_login("invalid_credentials")
     # Enable CSRF protection unless it will break the authenticator
     if not auth_settings.AUTHENTICATOR.uses_crossdomain_post_requests:
         handle_request = csrf_protect(handle_request)
@@ -125,7 +171,7 @@ def logout(request):
         if next_url:
             return redirect(next_url)
         else:
-            return redirect_to_login('logout_successful')
+            return redirect_to_login("logout_successful")
     else:
         # On GET requests, show the confirm page
         return render(request, "azimuth_auth/logout_confirm.html", dict(next_url = next_url))
