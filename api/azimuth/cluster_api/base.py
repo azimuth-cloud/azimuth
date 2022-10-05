c40243e5c08b202d75cc1d30cf65992915743a17
Matt Pryor <matt@stackhpc.com>
2022-10-05 18:55:01 +0100

    Revert unnecessary change

--- api/azimuth/cluster_api/base.py
+++ api/azimuth/cluster_api/base.py
@@ -27,7 +27,7 @@ logger = logging.getLogger(__name__)
 
 
 # Get the easykube configuration from the environment
-ekconfig = Configuration.from_environment(default_field_manager = "azimuth")
+ekconfig = Configuration.from_environment()
 
 
 ClusterTemplate = ResourceSpec(
