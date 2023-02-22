db444c2e5c4c14d458d9ffd030a1f0f061a03b6e
Matt Pryor <matt@stackhpc.com>
2023-02-22 13:56:50 +0000

    Add service FQDN popover

--- ui/src/components/pages/tenancy/platforms/utils.js
+++ ui/src/components/pages/tenancy/platforms/utils.js
@@ -1,17 +1,27 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import Col from 'react-bootstrap/Col';
+import FormControl from 'react-bootstrap/FormControl';
+import FormGroup from 'react-bootstrap/FormGroup';
+import FormText from 'react-bootstrap/FormText';
 import Image from 'react-bootstrap/Image';
+import InputGroup from 'react-bootstrap/InputGroup';
 import ListGroup from 'react-bootstrap/ListGroup';
 import Modal from 'react-bootstrap/Modal';
+import Overlay from 'react-bootstrap/Overlay';
+import Popover from 'react-bootstrap/Popover';
 import Row from 'react-bootstrap/Row';
+import Tooltip from 'react-bootstrap/Tooltip';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faBookmark,
+    faCheck,
+    faCopy,
     faExternalLinkAlt,
+    faPaste,
     faSyncAlt,
     faTrash
 } from '@fortawesome/free-solid-svg-icons';
@@ -44,16 +54,85 @@ export const PlatformTypeCard = ({ platformType }) => (
 );
 
 
-export const PlatformServicesListGroup = ({ services, disabled }) => (
-    <ListGroup variant="flush" activeKey={null}>
-        {services.map(service => (
+const PlatformServiceCopyLinkButton = ({ service }) => {
+    const [showCopied, setShowCopied] = useState(false);
+    const target = useRef(null);
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(service.fqdn);
+        setShowCopied(true);
+        setTimeout(() => setShowCopied(false), 3000);
+    };
+
+    return (
+        <>
+            <Button
+                ref={target}
+                title="Copy to clipboard"
+                variant="secondary"
+                className="service-popover-copy-btn"
+                onClick={handleCopy}
+            >
+                <FontAwesomeIcon icon={showCopied ? faCheck : faPaste} fixedWidth />
+            </Button>
+            <Overlay
+                target={target.current}
+                show={showCopied}
+                placement="top"
+            >
+                <Tooltip>Copied!</Tooltip>
+            </Overlay>
+        </>
+    );
+};
+
+
+const PlatformServicePopover = React.forwardRef(
+    ({ service, onLinkFollowed, ...props }, ref) => (
+        <Popover ref={ref} {...props} className="service-popover">
+            <Popover.Header>{service.label}</Popover.Header>
+            <Popover.Body>
+                <FormGroup>
+                    <InputGroup>
+                        <FormControl
+                            aria-label={`${service.label} FQDN`}
+                            disabled
+                            value={service.fqdn}
+                        />
+                        <PlatformServiceCopyLinkButton service={service} />
+                        <Button
+                            title="Go to service"
+                            variant="info"
+                            href={service.url}
+                            target="_blank"
+                            onClick={onLinkFollowed}
+                        >
+                            <FontAwesomeIcon icon={faExternalLinkAlt} fixedWidth />
+                        </Button>
+                    </InputGroup>
+                    <FormText>
+                        The URL for the service.<br />
+                        You can either visit the service directly or copy the URL to share with others.
+                    </FormText>
+                </FormGroup>
+            </Popover.Body>
+        </Popover>
+    )
+);
+
+
+const PlatformServiceListItem = ({ service, disabled }) => {
+    const [showPopover, setShowPopover] = useState(false);
+    const target = useRef(null);
+
+    return (
+        <>
             <ListGroup.Item
-                key={service.name}
+                ref={target}
                 action
-                href={service.url}
                 disabled={disabled}
-                target="_blank"
                 className="service-list-group-item"
+                onClick={() => setShowPopover(!showPopover)}
             >
                 <span>
                     {service.icon_url ? (
@@ -65,6 +144,31 @@ export const PlatformServicesListGroup = ({ services, disabled }) => (
                 <span>{service.label}</span>
                 <span><FontAwesomeIcon icon={faExternalLinkAlt} /></span>
             </ListGroup.Item>
+            <Overlay
+                target={target.current}
+                show={showPopover}
+                placement="top"
+                rootClose
+                onHide={() => setShowPopover(false)}
+            >
+                <PlatformServicePopover
+                    service={service}
+                    onLinkFollowed={() => setShowPopover(false)}
+                />
+            </Overlay>
+        </>
+    );
+};
+
+
+export const PlatformServicesListGroup = ({ services, disabled }) => (
+    <ListGroup variant="flush" activeKey={null}>
+        {services.map(service => (
+            <PlatformServiceListItem
+                key={service.name}
+                service={service}
+                disabled={disabled}
+            />
         ))}
     </ListGroup>
 );
