c39ddc72c3695c35e17dc71ca5beecd819b93a7b
John Garbutt <john.garbutt@stackhpc.com>
2024-01-24 12:02:06 +0000

    Rename shares to share

--- api/azimuth/provider/openstack/api/__init__.py
+++ api/azimuth/provider/openstack/api/__init__.py
@@ -1,3 +1,3 @@
 from .core import Connection, ServiceNotSupported
 # Import the modules for each of the services
-from . import block_store, coe, compute, identity, image, network, orchestration, shares
+from . import block_store, coe, compute, identity, image, network, orchestration, share
