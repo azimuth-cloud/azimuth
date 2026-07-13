413d5894aedb165ae3199931a8c5c41f2454383b
Ivan Anfimov <10336636+anfimovir@users.noreply.github.com>
2026-07-13 08:22:41 +0500

    block_store service renamed to block_storage
    The block_store service object has been renamed to block_storage to align the API with the official service types.

--- api/azimuth/provider/openstack/api/block_store.py
+++ api/azimuth/provider/openstack/api/block_store.py
@@ -48,7 +48,7 @@ class BlockStoreService(Service):
     OpenStack service class for the block store service.
     """
 
-    name = "block_store"
+    name = "block_storage"
     catalog_type = "block-storage"
     path_prefix = "/v3/{project_id}"
 
