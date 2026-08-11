c60d01bb874bf68797678ff9a7089962a2f00088
Alex Manning <alexm@stackhpc.com>
2026-08-11 16:07:53 +0100

    Max cluster lifetime (#640)

--- api/azimuth/provider/openstack/provider.py
+++ api/azimuth/provider/openstack/provider.py
@@ -1225,9 +1225,11 @@ class ScopedSession(base.ScopedSession):
         params = dict(
             security_group_id=secgroup.id,
             ethertype="IPv4",
-            direction="ingress"
-            if direction is dto.FirewallRuleDirection.INBOUND
-            else "egress",
+            direction=(
+                "ingress"
+                if direction is dto.FirewallRuleDirection.INBOUND
+                else "egress"
+            ),
         )
         if protocol != dto.FirewallRuleProtocol.ANY:
             params.update(protocol=protocol.name.lower())
--- api/tests/test_placeholder.py
+++
@@ -1,6 +0,0 @@
-import unittest
-
-
-class PlaceholderTest(unittest.TestCase):
-    def test_placeholder(self):
-        self.assertTrue(True)
